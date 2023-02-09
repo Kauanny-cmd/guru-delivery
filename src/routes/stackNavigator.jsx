932c358278c31a17d477b105947fb7d7acdcdf19
kauanny-cmd <kauannyvieira8@gmail.com>
2023-02-09 11:32:37 -0300

    feat: add novas telas

--- src/routes/stackNavigator.jsx
+++ src/routes/stackNavigator.jsx
@@ -1,14 +1,51 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Colors from '../global/style'
+
 import Login from "../screens/Login";
+import Register from '../screens/Register';
+import ForgetPassword from '../screens/ForgetPassword';
+import EmailSend from '../screens/EmailSend';
+import Welcome from '../screens/Welcome';
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
     return (
 
-        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Login" component={Login} />
+        <Stack.Navigator initialRouteName="Welcome"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: Colors.whiteColor,
+                },
+                headerTintColor: Colors.primaryColor
+            }}
+        >
+            <Stack.Screen name="Login" component={Login}
+                options={{
+                    headerTitle: '',
+                }}
+            />
+            <Stack.Screen name='Register' component={Register}
+                options={{
+                    headerTitle: '',
+                }}
+            />
+            <Stack.Screen name='ForgetPassword' component={ForgetPassword}
+                options={{
+                    headerTitle: '',
+                }}
+            />
+            <Stack.Screen name='EmailSend' component={EmailSend}
+                options={{
+                    headerTitle: '',
+                }}
+            />
+            <Stack.Screen name='Welcome' component={Welcome}
+                options={{
+                    headerShown: false
+                }}
+            />
         </Stack.Navigator>
     );
 }
